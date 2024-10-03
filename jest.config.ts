9b520276a0d2bed786c64d58262b75d70e9c9e40
Alexander Nedomansky <anedomansky@gmail.com>
2024-10-03 17:06:14 +0200

    Update deps
    - Updated dependencies
    - Updated jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,12 @@
-export default {
+import { Config } from "jest";
+
+const config: Config = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/src/.*/files/"],
-  testEnvironment: "jsdom",
+  testEnvironment: "jest-environment-jsdom",
   reporters: ["default"],
+  roots: ["<rootDir>/src"],
 };
+
+export default config;
