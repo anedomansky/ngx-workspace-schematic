333ab7b32c651ebea04a1e1e86dfd17aa67843de
Alexander Nedomansky <anedomansky@protonmail.com>
2025-05-26 20:18:37 +0200

    style(eslint): Config
    - Adds @anedomansky/eslint-config
    - Updates config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@
 import tseslint from "typescript-eslint";
 import { anStandardTS, anStandardHTML, anStandardJSON } from '@anedomansky/eslint-config';
 
-
 export default tseslint.config(
     ...anStandardTS,
     ...anStandardHTML,
