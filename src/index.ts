04ce722c0b329be318a92b9d7a02c4ee844f433b
Alexander Nedomansky <anedomansky@protonmail.com>
2025-06-06 20:20:52 +0200

    feat: File templates
    - Updates contents of file templates

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export * from "./ng-new";
-export * from "./application";
-export * from "./library";
-export * from "./workspace";
+export * from './application';
+export * from './library';
+export * from './ng-new';
+export * from './workspace';
+// TODO: test generated workspace
+// TODO: try to migrate from jest to vitest
