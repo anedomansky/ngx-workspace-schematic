07755c91f362ffc711587f3937a1ea4b90209b3d
Alexander Nedomansky <anedomansky@protonmail.com>
2025-06-08 18:02:55 +0200

    chore: TODOs
    - Removes TODO comments

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,3 @@ export * from './application';
 export * from './library';
 export * from './ng-new';
 export * from './workspace';
-// TODO: test generated workspace
-// TODO: try to migrate from jest to vitest
